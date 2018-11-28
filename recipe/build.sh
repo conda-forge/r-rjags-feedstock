7e35ca6de7ad1c9b07c48d4759abef7e8cb093b8
dpryan79 <dpryan79@gmail.com>
2018-11-28 11:19:12 +0100

    rpath

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
-  $R CMD INSTALL --build .
+  $R CMD INSTALL --configure-args='--enable-rpath' --build .
 else
   mkdir -p $PREFIX/lib/R/library/rjags
   mv * $PREFIX/lib/R/library/rjags
