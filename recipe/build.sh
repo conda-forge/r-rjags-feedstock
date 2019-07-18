c48d0343c8403697edb42e976424925d7d425993
Devon Ryan <dpryan79@gmail.com>
2019-07-19 00:33:44 +0200

    Odd that it's not finding the module, look for it

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
-  $R CMD INSTALL . #--configure-args='--enable-rpath' --build .
+  find $PREFIX -name basemod* -print
+  $R CMD INSTALL --configure-args='--enable-rpath' --build .
 else
   mkdir -p $PREFIX/lib/R/library/rjags
   mv * $PREFIX/lib/R/library/rjags
