5cd8ea631a5e4086ecc68ee1b3fcf990675e490b
Uwe L. Korn <uwe.korn@quantco.com>
2019-07-26 15:13:55 +0200

    Patch dynlib.ext on OSX

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+if [[ $target_platform == osx-64 ]]; then
+  sed -ie 's/.Platform\$dynlib.ext/".so"/g' R/jags.R
+fi
+
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
   find $PREFIX -name basemod* -print
