dce3c13d547ae28b72a651ebee6942eb893302d8
Mervin Fansler <mmfansler@gmail.com>
2024-07-25 20:05:04 +0200

    debug

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [[ $target_platform == osx-64 ]]; then
+if [[ $target_platform ~= osx-* ]]; then
   sed -ie 's/.Platform\$dynlib.ext/".so"/g' R/jags.R
 fi
 
-if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
-  export DISABLE_AUTOBREW=1
-  find $PREFIX -name basemod* -print
-  # shellcheck disable=SC2086
-  $R CMD INSTALL --configure-args='--enable-rpath' --build . ${R_ARGS}
-else
-  mkdir -p $PREFIX/lib/R/library/rjags
-  mv * $PREFIX/lib/R/library/rjags
-fi
+export DISABLE_AUTOBREW=1
+find $PREFIX -name basemod* -print
+# shellcheck disable=SC2086
+$R CMD INSTALL --configure-args='--enable-rpath' --build . ${R_ARGS}
+
+## BEGIN DEBUG
+cat src/Makevars
+cat R/unix/zzz.R
+## END DEBUG
