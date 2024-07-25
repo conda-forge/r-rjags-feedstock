54866c237f1b7649e7306f4b35f26ca668e50cfc
Mervin Fansler <mmfansler@gmail.com>
2024-07-25 19:38:48 +0200

    revert

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-export DISABLE_AUTOBREW=1
+if [[ $target_platform == osx-64 ]]; then
+  sed -ie 's/.Platform\$dynlib.ext/".so"/g' R/jags.R
+fi
 
-# shellcheck disable=SC2086
-${R} CMD INSTALL --build . ${R_ARGS}
+if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
+  export DISABLE_AUTOBREW=1
+  find $PREFIX -name basemod* -print
+  # shellcheck disable=SC2086
+  $R CMD INSTALL --configure-args='--enable-rpath' --build . ${R_ARGS}
+else
+  mkdir -p $PREFIX/lib/R/library/rjags
+  mv * $PREFIX/lib/R/library/rjags
+fi
